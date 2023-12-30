f684fb57b0510c1ac274e7ae9afb535ab54e320a
WonSeola <39373661+wonseola@users.noreply.github.com>
2023-12-30 20:28:33 +0900

    Update App.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return <Router>
     <Routes>
       <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+      <Route path="/movie/:id" element={<Detail />} />
     </Routes>
   </Router>;
 }
