4054d8f03d8ab8dd7d288d643fbf282343365102
WonSeola <39373661+wonseola@users.noreply.github.com>
2023-12-30 21:24:43 +0900

    css

--- app1/src/components/Movie.js
+++ app1/src/components/Movie.js
@@ -1,10 +1,11 @@
 
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom";
+import "../style.css"
 
 function Movie({ id, coverImg, title, summary, genres }) {
     return (
-        <div>
+        <div className="movie">
             <img src={coverImg} alt={title} />
             <h3><Link to={`${process.env.PUBLIC_URL}/movie/${id}`} > {title} </Link> </h3>
             <p>{summary}</p>
