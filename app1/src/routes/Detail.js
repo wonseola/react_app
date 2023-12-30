0f8816070dee80244a8f19ebbbbb7198d24b68ef
WonSeola <39373661+wonseola@users.noreply.github.com>
2023-12-30 19:56:57 +0900

    Update Detail.js

--- app1/src/routes/Detail.js
+++ app1/src/routes/Detail.js
@@ -1,8 +1,9 @@
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
+
     const { id } = useParams();
     const getMovie = async () => {
         const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
@@ -14,7 +15,7 @@ function Detail() {
     useEffect(() => {
         getMovie();
     }, [])
-    console.log(id);
+    // console.log(id);
     return <h1> Detail </h1>
 }
 export default Detail;
